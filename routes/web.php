d0d2f22b6bb056cb6f5b8ca65337cb187e268498
Liana <li13240897@gmail.com>
2024-10-02 08:20:09 +0300

    1.a

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view(' Tickets', ['Tickets' => [
+        ["theme" => "Сломался принтер", "email" => "lexa@mail.loc","date" => "2024-09-15"],
+        ["theme" => "Не включается компьютер", "email" => "babanura@mail.loc","date" => "2024-09-16"],
+        ["theme" => "Забыл пароль", "email" => "borkwork@mail.loc", "date" => "2024-09-17"],
+    ]]);
+});
